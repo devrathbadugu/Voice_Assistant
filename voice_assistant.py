7143f7b3578c8a9fd99e7264093f57ea7f00f1f4
Devrath Badugu <126484395+devrathbadugu@users.noreply.github.com>
2024-08-18 21:55:51 +0530

    voice_assistant.py

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,94 +1,60 @@
-import speech_recognition as sr
+import speech_recognition as sr 
 import datetime
 import subprocess
 import pywhatkit
 import pyttsx3
 import webbrowser
+import sys
+import psutil  # Import psutil to check battery status
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 recognizer = sr.Recognizer()
 
+# Define lambda functions for each command
+open_chrome = lambda text: 'chrome' in text and (engine.say('Opening chrome..'), engine.runAndWait(), subprocess.Popen(["C:\Program Files\Google\Chrome\Application\chrome.exe"]))
+tell_time = lambda text: 'time' in text and (lambda t: (print(t), engine.say(t), engine.runAndWait()))(datetime.datetime.now().strftime('%I:%M %p'))
+tell_date = lambda text: 'date' in text and (lambda d: (print(d), engine.say(d), engine.runAndWait()))(datetime.datetime.now().strftime('%B %d, %Y'))
+play_on_youtube = lambda text: 'play' in text and (engine.say('Opening YouTube..'), engine.runAndWait(), pywhatkit.playonyt(text))
+open_youtube = lambda text: 'youtube' in text and (engine.say('Opening YouTube..'), engine.runAndWait(), webbrowser.open('www.youtube.com'))
+exit_program = lambda text: 'exit' in text and (engine.say('Exiting program...'), engine.runAndWait(), sys.exit())
+tell_name = lambda text: 'name' in text and (engine.say("My name is Rosie."), engine.runAndWait())
+how_are_you = lambda text: 'how are you' in text and (engine.say("I'm just a program, but I'm here to help you!"), engine.runAndWait())
+open_facebook = lambda text: 'facebook' in text and (engine.say('Opening Facebook..'), engine.runAndWait(), webbrowser.open('http://www.facebook.com'))
+open_instagram = lambda text: 'instagram' in text and (engine.say('Opening Instagram..'), engine.runAndWait(), webbrowser.open('http://www.instagram.com'))
+open_github = lambda text: 'github' in text and (engine.say('Opening GitHub..'), engine.runAndWait(), webbrowser.open('http://www.github.com'))
+open_linkedin = lambda text: 'linkedin' in text and (engine.say('Opening LinkedIn..'), engine.runAndWait(), webbrowser.open('http://www.linkedin.com'))
+check_battery = lambda text: 'battery' in text and (lambda b: (print(f"Battery percentage: {b}%"), engine.say(f"Battery percentage is {b} percent"), engine.runAndWait()))(psutil.sensors_battery().percent)
+
+
 def cmd():
     with sr.Microphone() as source:
         print("Clearing background noises...Please wait")
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
         print('Ask me anything...')
-        recorded_audio = recognizer.listen(source)
+        recordedaudio = recognizer.listen(source)
     try:
-        text = recognizer.recognize_google(recorded_audio, language='en_US')
-        text = text.lower()
+        text = recognizer.recognize_google(recordedaudio, language='en_US').lower()
         print('Your message:', format(text))
+
+        # Execute the appropriate command using the lambda functions
+        open_chrome(text)
+        tell_time(text)
+        tell_date(text)
+        play_on_youtube(text)
+        open_youtube(text)
+        exit_program(text)
+        tell_name(text)
+        how_are_you(text)
+        open_facebook(text)
+        open_instagram(text)
+        open_github(text)
+        open_linkedin(text)
+        check_battery(text)
 
     except Exception as ex:
         print(ex)
-        text = ""  # Define a default value for text
-
-    if 'chrome' in text:
-        a = 'Opening chrome...'
-        engine.say(a)
-        engine.runAndWait()
-        program = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-        subprocess.Popen([program])
-    elif 'time' in text:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        print('Current time', time)
-        engine.say(time)
-        engine.runAndWait()
-    elif 'date' in text:
-        date = datetime.datetime.now().strftime('%d /%m /%Y')
-        print('Todays date', date)
-        engine.say(date)
-        engine.runAndWait()
-    elif 'play' in text:
-        b = 'Opening YouTube...'
-        engine.say(b)
-        engine.runAndWait()
-        pywhatkit.playonyt(text)
-    elif 'youtube' in text:
-        b = 'Opening YouTube'
-        engine.say(b)
-        engine.runAndWait()
-        webbrowser.open('www.youtube.com')
-    elif 'how are you' in text:
-        response = 'I am fine, how about you'
-        print(response)
-        engine.say(response)
-        engine.runAndWait()
-    elif 'what is your name' in text:
-        response = 'I am Siri, what can I do for you?'
-        print(response)
-        engine.say(response)
-        engine.runAndWait()
-    elif 'exit' in text:
-        print("Exiting the program...")
-        engine.say("Exiting the program...")
-        engine.runAndWait()
-        return False  # Return False to break the loop
-    elif 'instagram' in text:
-        c = 'Opening Instagram'
-        engine.say(c)
-        engine.runAndWait()
-        webbrowser.open('www.instagram.com')
-    elif 'linkedin' in text:
-        d = 'Opening LinkedIn'
-        engine.say(d)
-        engine.runAndWait()
-        webbrowser.open('www.linkedin.com')
-    elif 'github' in text:
-        e = 'Opening GitHub'
-        engine.say(e)
-        engine.runAndWait()
-        webbrowser.open('www.github.com')
-    elif 'facebook' in text:
-        f = 'Opening Facebook'
-        engine.say(f)
-        engine.runAndWait()
-        webbrowser.open('www.facebook.com')
-        
-    return True  # Return True to continue the loop
 
 while True:
-    if not cmd():
-        break
+    cmd()
